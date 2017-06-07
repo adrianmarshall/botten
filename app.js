60e3ae3629428b570869a33ad689fc7092ce1492
Adrian Marshall <adrian.marshall@ge.com>
2017-06-06 20:41:52 -0400

    port 3978

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ bot.dialog('help', function (session) {
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || 80, function () {
+server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
